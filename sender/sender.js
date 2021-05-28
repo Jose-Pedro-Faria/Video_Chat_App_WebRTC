ce15db4626ddc8efaaedc695da0b6d0d75ab9361
José Pedro Faria <josepedropfaria@gmail.com>
2021-05-28 11:02:31 +0100

    atualização

--- sender/sender.js
+++ sender/sender.js
@@ -1,4 +1,4 @@
-const webSocket = new WebSocket("ws://192.168.43.54:5500")
+const webSocket = new WebSocket("ws://172.18.6.200:5500")
 
 webSocket.onmessage = (event) => {
     handleSignallingData(JSON.parse(event.data))
